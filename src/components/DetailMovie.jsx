6f9ad7d2abc01c342c4c25798b81d3f43d0310c2
Jovianto Trisila <mandevil41@gmail.com>
2022-07-21 19:17:02 +0700

    Test

---
+++ src/components/DetailMovie.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from '../component/Footer'
+import DetailMovie from './containers/DetailMovie';
+
+export default function DetailFilm() {
+  return (
+    <div>
+        <DetailMovie/>
+        <Footer/>
+    </div>
+  )
+}
